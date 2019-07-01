8813a77bf790c1f2e31c343dce1806c38f375216
shisheng4215 <shisheng4215@163.com>
2019-07-01 23:56:59 +0800

    Moved 'fortune cookie' functionality into module.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var fortune = require('../lib/fortune.js');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home');
@@ -8,7 +11,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/about',function(req,res){
 	//var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-	res.render('about',{fortune:'my fortune'});
+	res.render('about',{fortune:fortune.getFortune()});
 });
 
 
